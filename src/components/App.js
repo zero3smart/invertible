d4ade8e5f1248374b20295e852ad453a39dd32fc
pokesoftech <fivewisdomzero@gmail.com>
2017-11-30 00:59:06 -0500

    remove unused props

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ class App extends Component {
     render() {
         return (
             <div className="app">
-                <Header a />
+                <Header />
                 <div className="app-body">
                     {/* <Sidebar {...this.props} /> */}
                     <main className="main">
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,6 @@ import { IndexLink } from 'react-router';
 class Header extends Component {
     constructor(props) {
         super(props);
-        console.log(props.a);
     }
     sidebarToggle(e) {
         e.preventDefault();
