8d9f52dac283a3e035c7753dbefbbcabecff4f46
billionsoftech <fivewisdomzero@gmail.com>
2017-12-27 15:25:13 -0500

    update data table

--- src/components/Dashboard2.js
+++ src/components/Dashboard2.js
@@ -40,7 +40,6 @@ class Dashboard2 extends Component {
                 'LandingPage': 'landingpath'
             },
             reportOptions: {
-                id: "",
                 total: "",
                 sessions: "",
                 transactions: "",
@@ -64,7 +63,7 @@ class Dashboard2 extends Component {
         return obj;
     }
 
-    getFilteredList(groupByAttr, value) {
+    getFilteredList(groupByAttr) {
         let _filteredList = [];
         let { analytics } = this.props;
 
@@ -113,7 +112,7 @@ class Dashboard2 extends Component {
 
         let { analytics } = this.props;
 
-        var rawDataValuesOne = this.getFilteredList('date', value);
+        var rawDataValuesOne = this.getFilteredList('date');
 
         let reportOptions = { ...this.state.reportOptions };
 
@@ -138,10 +137,26 @@ class Dashboard2 extends Component {
         reportOptions.averageTime = totalDuration / totalVisits / 86400;
 
         //Get reports data for barchart
-        var rawDataValuesTwo = this.getFilteredList(groupBy, value);
+        var rawDataValuesTwo = this.getFilteredList(groupBy);
 
         let analysisResult = [];
-        analysisResult.push(this.convertObjectToComma(reportOptions));
+        if (groupBy === 'total')
+            analysisResult.push(this.convertObjectToComma(reportOptions));
+        else
+            analysisResult = rawDataValuesTwo;
+
+        this.$el.DataTable({
+            destroy: true,
+            data: analysisResult,
+            columns: [
+                { "data": "xValue" },
+                { "data": "sessions" },
+                { "data": "transactions" },
+                { "data": "bounceRate" },
+                { "data": "conversionRate" },
+                { "data": "averageTime" }
+            ]
+        });
 
         this.setState({ analysisResult });
         this.setState({ reportOptions });
@@ -166,59 +181,8 @@ class Dashboard2 extends Component {
     }
 
     componentDidMount() {
-        this.fetchAnalyticsData();
         this.$el = $(this.analyticsTable);
-
-        var dataSet = [
-            ["Tiger Nixon", "System Architect", "Edinburgh", "5421", "2011/04/25", "$320,800"],
-            ["Garrett Winters", "Accountant", "Tokyo", "8422", "2011/07/25", "$170,750"],
-            ["Ashton Cox", "Junior Technical Author", "San Francisco", "1562", "2009/01/12", "$86,000"],
-            ["Cedric Kelly", "Senior Javascript Developer", "Edinburgh", "6224", "2012/03/29", "$433,060"],
-            ["Airi Satou", "Accountant", "Tokyo", "5407", "2008/11/28", "$162,700"],
-            ["Brielle Williamson", "Integration Specialist", "New York", "4804", "2012/12/02", "$372,000"],
-            ["Herrod Chandler", "Sales Assistant", "San Francisco", "9608", "2012/08/06", "$137,500"],
-            ["Rhona Davidson", "Integration Specialist", "Tokyo", "6200", "2010/10/14", "$327,900"],
-            ["Colleen Hurst", "Javascript Developer", "San Francisco", "2360", "2009/09/15", "$205,500"],
-            ["Sonya Frost", "Software Engineer", "Edinburgh", "1667", "2008/12/13", "$103,600"],
-            ["Jena Gaines", "Office Manager", "London", "3814", "2008/12/19", "$90,560"],
-            ["Quinn Flynn", "Support Lead", "Edinburgh", "9497", "2013/03/03", "$342,000"],
-            ["Charde Marshall", "Regional Director", "San Francisco", "6741", "2008/10/16", "$470,600"],
-            ["Haley Kennedy", "Senior Marketing Designer", "London", "3597", "2012/12/18", "$313,500"],
-            ["Tatyana Fitzpatrick", "Regional Director", "London", "1965", "2010/03/17", "$385,750"],
-            ["Michael Silva", "Marketing Designer", "London", "1581", "2012/11/27", "$198,500"],
-            ["Paul Byrd", "Chief Financial Officer (CFO)", "New York", "3059", "2010/06/09", "$725,000"],
-            ["Gloria Little", "Systems Administrator", "New York", "1721", "2009/04/10", "$237,500"],
-            ["Bradley Greer", "Software Engineer", "London", "2558", "2012/10/13", "$132,000"],
-            ["Dai Rios", "Personnel Lead", "Edinburgh", "2290", "2012/09/26", "$217,500"],
-            ["Jenette Caldwell", "Development Lead", "New York", "1937", "2011/09/03", "$345,000"],
-            ["Yuri Berry", "Chief Marketing Officer (CMO)", "New York", "6154", "2009/06/25", "$675,000"],
-            ["Caesar Vance", "Pre-Sales Support", "New York", "8330", "2011/12/12", "$106,450"],
-            ["Doris Wilder", "Sales Assistant", "Sidney", "3023", "2010/09/20", "$85,600"],
-            ["Angelica Ramos", "Chief Executive Officer (CEO)", "London", "5797", "2009/10/09", "$1,200,000"],
-            ["Gavin Joyce", "Developer", "Edinburgh", "8822", "2010/12/22", "$92,575"],
-            ["Jennifer Chang", "Regional Director", "Singapore", "9239", "2010/11/14", "$357,650"],
-            ["Brenden Wagner", "Software Engineer", "San Francisco", "1314", "2011/06/07", "$206,850"],
-            ["Fiona Green", "Chief Operating Officer (COO)", "San Francisco", "2947", "2010/03/11", "$850,000"],
-            ["Shou Itou", "Regional Marketing", "Tokyo", "8899", "2011/08/14", "$163,000"],
-            ["Michelle House", "Integration Specialist", "Sidney", "2769", "2011/06/02", "$95,400"],
-            ["Suki Burks", "Developer", "London", "6832", "2009/10/22", "$114,500"],
-            ["Prescott Bartlett", "Technical Author", "London", "3606", "2011/05/07", "$145,000"],
-            ["Gavin Cortez", "Team Leader", "San Francisco", "2860", "2008/10/26", "$235,500"],
-            ["Martena Mccray", "Post-Sales support", "Edinburgh", "8240", "2011/03/09", "$324,050"],
-            ["Unity Butler", "Marketing Designer", "San Francisco", "5384", "2009/12/09", "$85,675"]
-        ];
-
-        this.$el.DataTable({
-            data: dataSet,
-            columns: [
-                { title: "Name" },
-                { title: "Position" },
-                { title: "Office" },
-                { title: "Extn." },
-                { title: "Start date" },
-                { title: "Salary" }
-            ]
-        });
+        this.fetchAnalyticsData();
     }
 
     rateFormatter(cell) {
@@ -508,11 +472,21 @@ class Dashboard2 extends Component {
                                 </button>
                             </div>
                             <div className="table-block">
-                                <div>
+                                <div className="">
                                     <table className="ui celled table"
                                         cellSpacing="0"
                                         ref={(el) => this.analyticsTable = el}
                                         width="100%">
+                                        <thead>
+                                            <tr>
+                                                <th>{this.state.group2Active}</th>
+                                                <th>Sessions</th>
+                                                <th>Transactions</th>
+                                                <th>Bounce Rate</th>
+                                                <th>Conversion Rate</th>
+                                                <th>Average Time Spent on Site</th>
+                                            </tr>
+                                        </thead>
                                     </table>
                                 </div>
                                 {/* {!this.state.loading &&
