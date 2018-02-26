493e017f6538d0398ee6bdac8ced8c34b877644d
billionsoftech <fivewisdomzero@gmail.com>
2018-02-26 17:21:32 -0500

    add stack bar chart

--- src/components/Funnel.js
+++ src/components/Funnel.js
@@ -227,6 +227,104 @@ class Funnel extends Component {
                 }} />
         );
 
+        const funnelStackBarChart = (
+            <AmCharts.React
+                style={{
+                    width: "100%",
+                    height: "400px"
+                }}
+                options={{
+                    "type": "serial",
+                    "theme": "light",
+                    "rotate": true,
+                    "marginBottom": 50,
+                    "dataProvider": [{
+                        "age": "85+",
+                        "male": -0.1,
+                        "female": 0.3
+                    }, {
+                        "age": "80-54",
+                        "male": -0.2,
+                        "female": 0.3
+                    }, {
+                        "age": "75-79",
+                        "male": -0.3,
+                        "female": 0.6
+                    }],
+                    "startDuration": 1,
+                    "graphs": [{
+                        "fillAlphas": 0.8,
+                        "lineAlpha": 0.2,
+                        "type": "column",
+                        "valueField": "male",
+                        "title": "Male",
+                        "labelText": "[[value]]",
+                        "clustered": false,
+                        "labelFunction": function (item) {
+                            return Math.abs(item.values.value);
+                        },
+                        "balloonFunction": function (item) {
+                            return item.category + ": " + Math.abs(item.values.value) + "%";
+                        }
+                    }, {
+                        "fillAlphas": 0.8,
+                        "lineAlpha": 0.2,
+                        "type": "column",
+                        "valueField": "female",
+                        "title": "Female",
+                        "labelText": "[[value]]",
+                        "clustered": false,
+                        "labelFunction": function (item) {
+                            return Math.abs(item.values.value);
+                        },
+                        "balloonFunction": function (item) {
+                            return item.category + ": " + Math.abs(item.values.value) + "%";
+                        }
+                    }],
+                    "categoryField": "age",
+                    "categoryAxis": {
+                        "gridPosition": "start",
+                        "gridAlpha": 0.2,
+                        "axisAlpha": 0
+                    },
+                    "valueAxes": [{
+                        "gridAlpha": 0,
+                        "ignoreAxisWidth": true,
+                        "labelFunction": function (value) {
+                            return Math.abs(value) + '%';
+                        },
+                        "guides": [{
+                            "value": 0,
+                            "lineAlpha": 0.2
+                        }]
+                    }],
+                    "balloon": {
+                        "fixedPosition": true
+                    },
+                    "chartCursor": {
+                        "valueBalloonsEnabled": false,
+                        "cursorAlpha": 0.05,
+                        "fullWidth": true
+                    },
+                    "allLabels": [{
+                        "text": "Male",
+                        "x": "28%",
+                        "y": "97%",
+                        "bold": true,
+                        "align": "middle"
+                    }, {
+                        "text": "Female",
+                        "x": "75%",
+                        "y": "97%",
+                        "bold": true,
+                        "align": "middle"
+                    }],
+                    "export": {
+                        "enabled": true
+                    }
+                }} />
+        );
+
         return (
             <div className="funnel-container">
                 <div className="row">
@@ -330,6 +428,9 @@ class Funnel extends Component {
                 </div>
                 <div className="row">
                     { this.state.loading ? loading : funnelLineChart }
+                </div>
+                <div className="row">
+                    { this.state.loading ? loading : funnelStackBarChart }
                 </div>
             </div>
         );
